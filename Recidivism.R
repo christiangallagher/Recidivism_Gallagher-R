bbc77917d079a2982cadf57ceb0eea9ebdfdb90c
christian gallagher <cgallag6@students.kennesaw.edu>
2021-10-04 18:39:54 -0400

    Updated chart

--- Recidivism.R
+++ Recidivism.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(ggplot2)
 library(dbplyr)
 df <- read.csv(file = '~/Documents/NIJ_s_Recidivism_Challenge_Full_Dataset.csv')
+df$Gender = ifelse(df$Gender == "F", "Female", ifelse(df$Gender == "M", "Male", NA))
 data_pop <- tibble(
   Race = c("BLACK", "WHITE"), 
   N =  331449281 *c(.124, .616))
@@ -19,6 +20,9 @@ l <- data_counts %>%
 
 l %>%
   ggplot(aes(x = Age_at_Release, y = (n/N)*100, color = Race)) +
-  geom_line(aes(group = Race)) + facet_grid(cols = vars(Gender))
+  geom_line(aes(group = Race)) + facet_grid(cols = vars(Gender)) +
+  labs(x = "Age at Release", y = "Proportional Population of Released Inmates", 
+       title = "2021 Inmates Showing Age of Release Grouped by Race" ) + 
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5), axis.text.x = element_text(angle = 90))
                                             
 
